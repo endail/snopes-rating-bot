7f785724415fe42380adbfb3a4778b27d175e718
endail <52652357+endail@users.noreply.github.com>
2022-02-15 20:09:34 +1100

    fix db test

--- tests/db.py
+++ tests/db.py
@@ -4,6 +4,5 @@ if __name__ == "__main__":
   sys.path.insert(0, './src')
   from store import store
 
-  store.set_last_twitter_id("1492326600221863937")
   print(store.get_oauth_token())
   print(store.get_last_twitter_id())
