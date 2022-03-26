e7e75237dda062133e7bfe545b788513ff608b94
endail <52652357+endail@users.noreply.github.com>
2022-03-26 20:59:07 +1100

    fix git return val

--- update.bash
+++ update.bash
@@ -2,4 +2,5 @@
 pip install --upgrade pip;
 poetry install --no-root;
 
-git add -A && git commit -am 'install packages';
+git add -A
+git diff-index --quiet HEAD || git commit -am 'install packages';
