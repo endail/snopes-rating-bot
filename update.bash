9e91c52d0cacc9bb8ed54b5804d354fc77d1dda6
endail <52652357+endail@users.noreply.github.com>
2022-04-01 23:41:48 +1100

    fix update

--- update.bash
+++ update.bash
@@ -1,12 +1,28 @@
 
-rm -rf /opt/virtualenvs/python3/lib/python3.8/site-packages/*;
+LOCAL_SP="$HOME/snopes-rating-bot/site-packages";
+SYSTEM_SP=$(python -c 'import sysconfig; print(sysconfig.get_paths()["purelib"])')
 
-PYTHONPATH=site-packages;
+rm -rf "$LOCAL_SP"/*;
+rm -rf "$SYSTEM_SP"/*;
+
+export PYTHONPATH="$LOCAL_SP";
 
 python -m ensurepip;
 
-until poetry install --no-dev
-do 
-  echo "restarting poetry install";
-  sleep 1;
-done
+pip config set global.target "$LOCAL_SP";
+pip config set user.target "$LOCAL_SP";
+pip config set site.target "$LOCAL_SP";
+
+python -m pip install --upgrade --force-reinstall pip;
+python -m pip install --upgrade --force-reinstall setuptools;
+python -m pip install --upgrade --force-reinstall poetry;
+
+poetry install --no-dev
+#until poetry install --no-dev
+#do 
+#  echo "restarting poetry install";
+#  sleep 1;
+#done
+
+git add -A;
+git diff-index --quiet HEAD || git commit -am "install packages";
