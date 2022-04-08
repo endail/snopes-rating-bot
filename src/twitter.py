d3015a001adcaba41c298e37a1cfc6f31cb2b424
endail <52652357+endail@users.noreply.github.com>
2022-04-08 17:05:42 +1000

    fix twitter hashtag

--- src/twitter.py
+++ src/twitter.py
@@ -38,7 +38,7 @@ class twitter:
   def is_valid_hashtag(tag: str) -> bool:
     # https://stackoverflow.com/a/36902556/570787
     # need to use regex module (not re) for unicode support, spec \p{L}
-    pattern = r'(?:^|[^&\p{L}\p{M}\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7])(?:#|\uFF03)(?!\uFE0F|\u20E3)(?:[\p{L}\p{M}\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7]*[\p{L}\p{M}][\p{L}\p{M}\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7]*)'
+    pattern = r'(?:(?<=\s)|^)#(\w*[A-Za-z_]+\w*)'
     return bool(regex.match(pattern, tag))
 
   @staticmethod
