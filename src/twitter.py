2e0ce49ec548baa29e5f992c83359addb51e609a
endail <52652357+endail@users.noreply.github.com>
2022-04-08 17:01:46 +1000

    fix twitter hashtag regex

--- src/twitter.py
+++ src/twitter.py
@@ -38,7 +38,7 @@ class twitter:
   def is_valid_hashtag(tag: str) -> bool:
     # https://stackoverflow.com/a/36902556/570787
     # need to use regex module (not re) for unicode support, spec \p{L}
-    pattern = r'(^|\s)([#＃][\w\u05be\u05f3\u05f4]*[\p{L}_]+[\w\u05be\u05f3\u05f4]*)'
+    pattern = r'(?:^|[^&\p{L}\p{M}\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7])(?:#|\uFF03)(?!\uFE0F|\u20E3)(?:[\p{L}\p{M}\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7]*[\p{L}\p{M}][\p{L}\p{M}\p{Nd}_\u200c\u200d\ua67e\u05be\u05f3\u05f4\u309b\u309c\u30a0\u30fb\u3003\u0f0b\u0f0c\u00b7]*)'
     return bool(regex.match(pattern, tag))
 
   @staticmethod
