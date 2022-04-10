6d98d895f1218b8ff89d00497e234b934aee867b
endail <52652357+endail@users.noreply.github.com>
2022-04-10 21:08:14 +1000

    add update guard to run script

--- run.bash
+++ run.bash
@@ -5,9 +5,15 @@ source ~/.profile
 cd "$HOME/$APP_NAME";
 
 if pgrep -fl "bash update.bash" >/dev/null; then
-  #guard against running while updating
+  # guard against running while updating
   echo "$APP_NAME is updating";
   exit 1;
+fi
+
+# if tweepy doesn't exist, determine that to mean
+# an update is required
+if ! $(python -c "import tweepy" 2>/dev/null); then
+  bash update.bash;
 fi
 
 # check if python main.py is already running
